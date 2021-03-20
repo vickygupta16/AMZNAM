8a88b5ca19a9650795804530aca739fbdb42fd6b
Vicky Gupta <59506220+vickygupta16@users.noreply.github.com>
2021-03-20 21:00:21 +0530

    Add files via upload

---
+++ JS/myScript.js
@@ -0,0 +1,22 @@
+var mybutton = document.getElementById("myBtn");
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+window.onload = function () {
+  document.addEventListener("contextmenu", (event) => event.preventDefault());
+};
