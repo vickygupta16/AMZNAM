b745b6928a067fc48d4fbf2b9c1cdbcb0aa72255
Vicky Gupta <59506220+vickygupta16@users.noreply.github.com>
2021-04-07 20:18:22 +0530

    Update myScript.js

--- JS/myScript.js
+++ JS/myScript.js
@@ -1,22 +1,3 @@
-var mybutton = document.getElementById("myBtn");
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
 function displayProducts(id) {
   const data = {
     march22: {
